413f06d230bda97f0a02cfaa71436eb7ab7a8abe
Lusenomichael7 <zatiluseno@gmail.com>
2025-09-01 17:40:28 +0300

    Update Data Cleaning.sql

--- Data Cleaning.sql
+++ Data Cleaning.sql
@@ -1,1 +1,21 @@
+/*There are 211 duplicate rows */
+SELECT ride_id,start_station_name,end_station_name, COUNT(*) 
+FROM `plated-dryad-448619-k8.Cyclistic.Cyclistic_Combined_Table`
+GROUP BY ride_id,start_station_name,end_station_name
+HAVING COUNT(*) > 1;
+
+SELECT *
+FROM (
+ SELECT
+      *,
+      ROW_NUMBER()
+          OVER (PARTITION BY ride_id)
+          row_number
+  FROM `plated-dryad-448619-k8.Cyclistic.Cyclistic_Combined_Table`
+)
+WHERE row_number = 1
+/* After removing the duplicate rows, 5,860,357 rows remained out of the original 5,860,568 */
+
+
+
 
